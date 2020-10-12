e3013eafe3f6326ce08375edad9ec5be90777107
Marco Amoia <m.amoia3@studenti.uniba.it>
2020-10-12 15:02:31 +0200

    Splash screen
    MainActivity istanzia lo splash screen con un delay di 5000ms, poi richiama l’activity Dashboard (nome temporaneo)

---
+++ main/java/controllers/Dashboard.java
@@ -0,0 +1,16 @@
+package controllers;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.ristodroid.R;
+
+public class Dashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+}
