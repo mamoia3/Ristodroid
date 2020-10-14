931d4c5be297da051d600adb30a3b09f443a3707
Marco Amoia <m.amoia3@studenti.uniba.it>
2020-10-14 11:54:07 +0200

    Refactor splash screen
    Splash screen riscritto eliminando imageview e textview per alleggerire il codice. Implementata layer-list e nuovo tema (“splashTheme”).

--- main/java/controllers/MainActivity.java
+++ main/java/controllers/MainActivity.java
@@ -4,46 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.view.WindowManager;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.ImageView;
-import android.widget.TextView;
-
-import com.example.ristodroid.R;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static int SPLASH_SCREEN = 3000;
-
-    private Animation topAnim, bottomAnim;
-    private ImageView logo;
-    private TextView title;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_main);
 
-        topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation);
-        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+        try {
+            Thread.sleep(1000L);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
-        logo = (ImageView) findViewById(R.id.logoSplashScreen);
-        title = (TextView) findViewById(R.id.titleSplashScreen);
+        startActivity(new Intent(getApplicationContext(), Dashboard.class));
 
-        logo.setAnimation(bottomAnim);
-        title.setAnimation(topAnim);
-
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent intent = new Intent(MainActivity.this, Dashboard.class);
-                startActivity(intent);
-                finish();
-            }
-        }, SPLASH_SCREEN);
     }
 
 }
