58a7ca969e91810dcc64b5f2e80f64c1eeb634f8
Marco Amoia <m.amoia3@studenti.uniba.it>
2020-10-28 18:56:06 +0100

    Fix bottone indietro
    - Modificato il grafo di navigazione eliminando i popup behavior (eh si)
    - Eliminato metodo onOptionsItemSelected() perchè era inutile implementando onSupportNavigationUp()
    - Risolto problema di visualizzazione del titolo delle categorie dei piatti (veniva visualizzato fragment_dishes tornando indietro dal dettaglio del piatto) spostando la riga che modifica la stringa del titolo dal metodo onCreate al metodo onCreateView del fragment

--- main/java/controllers/Dashboard.java
+++ main/java/controllers/Dashboard.java
@@ -1,12 +1,15 @@
 package controllers;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
@@ -24,14 +27,12 @@ public class Dashboard extends AppCompatActivity  {
 
     private NavHostFragment navHostFragment;
     private BottomNavigationView navMenu;
+    private NavController navController;
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == android.R.id.home) {
-            navHostFragment.getNavController().navigateUp();
-            return true;
-        }
-        return false;
+    public boolean onSupportNavigateUp() {
+        return Navigation.findNavController(this, navHostFragment.getView().getId()).navigateUp()
+                || super.onSupportNavigateUp();
     }
 
     @Override
@@ -47,7 +48,7 @@ public class Dashboard extends AppCompatActivity  {
 
         navHostFragment = (NavHostFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.nav_host_fragment);
-        NavController navController = navHostFragment.getNavController();
+        navController = navHostFragment.getNavController();
 
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
